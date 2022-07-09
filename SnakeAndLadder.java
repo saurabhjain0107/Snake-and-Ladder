74fdf0354285edbfa6222fa76bd5a81ed1947a0c
saurabh jain <luckyjain0107@gmai.com>
2022-07-09 16:55:05 +0530

    Start Position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,5 +3,6 @@ package com.bridgelabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game !");
+        int Position=0;
     }
 }
