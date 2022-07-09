633ba49160e94bb19a8c10331c357ad42d130834
saurabh jain <luckyjain0107@gmai.com>
2022-07-09 16:59:09 +0530

    Roll the Dice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,5 +4,7 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game !");
         int Position=0;
+        int no=(int)(Math.random()*10)%6+1;
+        System.out.println(no);
     }
 }
