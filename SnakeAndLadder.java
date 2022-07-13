3d7f86a36de191a7dfdec222fcb453b2f2bf96f1
saurabh jain <luckyjain0107@gmai.com>
2022-07-13 18:42:44 +0530

    Exact Position Till100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,14 +1,15 @@
 package com.bridgelabz;
-
 public class SnakeAndLadder {
+
     static final int LADDER = 1;
     static final int SNAKE = 2;
     static final int NO_PLAY = 0;
-
-    public static void main(String[] args) {
+    public static void main (String[]args){
         System.out.println("Welcome to Snake and Ladder Game !");
         int Position = 0;
-        while (Position < 100) {
+        while(Position< 100)
+
+        {
             int dice = (int) (Math.random() * 10) % 6 + 1;
             System.out.println("dice " + dice);
             int OptionCheck = (int) (Math.random() * 10) % 3;
@@ -25,11 +26,12 @@ public class SnakeAndLadder {
                 Position = Position;
             }
             if (Position > 100) {
-                Position = 100;
+                Position = Position - dice;
             }
             if (Position < 0) {
                 Position = 0;
             }
+
             System.out.println("Position " + Position);
         }
     }
